488b2c895138e37e1de8121eedc39ca274163e49
cakirtufan <58190369+cakirtufan@users.noreply.github.com>
2025-09-20 02:41:59 +0200

    Update Forecasting_Action.py

--- Competitions/Forecasting_Action.py
+++ Competitions/Forecasting_Action.py
@@ -9,21 +9,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.inspection import permutation_importance
-
-
 class ForcastingAction:
     def __init__(self, train_path, test_path, gender_submission_path, model=None):
         self.train_path = train_path
         self.test_path = test_path
         self.sample_submission_path = sample_submission_path
-
-        # Default model: SVC if none is provided
-        self.model = model if model else SVC(probability=True)
 
         # Data placeholders
         self.train_df = None
